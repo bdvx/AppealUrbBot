2366ce2c571e9c915b07a6c5df1856b67036dbba
Denis_Beskorovainyi <besdenv@gmail.com>
2021-01-11 18:36:21 +0300

    feat: categories button panel

--- bot.py
+++ bot.py
@@ -1,19 +1,50 @@
 import os
 from dotenv import load_dotenv
-import telebot;
+import telebot
+from telebot import types
 
 load_dotenv('.env')
 
 bot = telebot.TeleBot(os.getenv('TG_TOKEN'));
+
+stringList = {}
+stringList[1] = {"Blago": "Благоустройство", "Soc": "Социальное направление"}
+stringList[2] = {"Opeka": "Опека", "Contacts": "Контакты"}
+stringList[3] = {"Alko": "Границы продажи алкогольной продукции"}
+
+def makeCategories():
+    markup = types.InlineKeyboardMarkup()
+    for key, value in stringList.items():
+        buttons = []
+        for k, val in value.items():
+                buttons.append(types.InlineKeyboardButton(text=val,
+                                                    callback_data="['value', '" + k + "', '" + k + "']"))
+                                            
+        markup.add(*buttons)
+
+    return markup
+
+
+def makeSubCategories():
+    markup = types.InlineKeyboardMarkup()
+    for key, value in stringList.items():
+        for k, val in value.items():
+            markup.add(types.InlineKeyboardButton(text=val,
+                                                callback_data="['value', '" + val + "', '" + k + "']"))
+
+    return markup
 
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
 
     if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        bot.send_message(chat_id=message.from_user.id,
+                     text="Выберите категорию:",
+                     reply_markup=makeCategories(),
+                     parse_mode='HTML')
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+        bot.send_message(message.from_user.id, "/new - Новое обращение")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
