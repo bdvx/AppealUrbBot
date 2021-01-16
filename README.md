9261fb0f81707fa2bb1e65f0d394033a14f82a09
Denis_Beskorovainyi <besdenv@gmail.com>
2021-01-17 00:24:02 +0300

    Categories and subCategories

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AppealUrbBot
 Telegram bot for the users appeal system
+
+Uses Heroku as a hosting platform
--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
 import os
-# from dotenv import load_dotenv
+#from dotenv import load_dotenv
+import webbrowser
 import telebot
 from telebot import types
 
-# load_dotenv('.env')
+# local setup
+#load_dotenv('.env')
 
 bot = telebot.TeleBot(os.environ.get('TG_TOKEN'));
 
-stringList = {}
-stringList[1] = {"Blago": "Благоустройство", "Soc": "Социальное направление"}
-stringList[2] = {"Opeka": "Опека", "Contacts": "Контакты"}
-stringList[3] = {"Alko": "Границы продажи алкогольной продукции"}
+catList = {}
+catList[1] = {"Blago": "Благоустройство", "Soc": "Социальное направление"}
+catList[2] = {"Opeka": "Опека", "Contacts": "Контакты"}
+catList[3] = {"Alko": "Границы продажи алкогольной продукции"}
 
-def makeCategories():
+def makeCategories(catList):
     markup = types.InlineKeyboardMarkup()
-    for key, value in stringList.items():
+    for key, value in catList.items():
         buttons = []
         for k, val in value.items():
-                buttons.append(types.InlineKeyboardButton(text=val,
-                                                    callback_data="['value', '" + k + "', '" + k + "']"))
+                buttons.append(types.InlineKeyboardButton(text=val, callback_data=k))
                                             
         markup.add(*buttons)
 
     return markup
 
 
-def makeSubCategories():
+def makeSubCategories(category):
     markup = types.InlineKeyboardMarkup()
     for key, value in stringList.items():
         for k, val in value.items():
@@ -34,14 +35,40 @@ def makeSubCategories():
 
     return markup
 
-@bot.message_handler(content_types=['text'])
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        subList = {}
+        topText = "Выберите подкатегорию"
+        if call.data == "Blago":
+            subList[1] = {"Uborka": "Уборка", "Oborud": "Оборудование", "Transport": "Транспорт"}
+            subList[2] = {"Zelen": "Зеленые насаждения", "Zamech": "Замечания от жителей"}
+        if call.data == "Soc":
+            subList[1] = {"Sport": "Спортивные мероприятия", "Semin": "Семинары, вебинары"}
+            subList[2] = {"Prazd": "Праздничные мероприятия"}
+            subList[3] = {"Trud": "Трудоустройство несовершеннолетних"}
+        if call.data == "Opeka":
+            topText += "\nПо иным вопросам Вы можете обратиться к сотруднику отдела опеки и попечительства по тел. 5282936"
+            subList[1] = {"Sdelk": "Сделки", "Opek": "Опека"}
+            subList[2] = {"Razresh": "Разрешение на трудоустройство несовершеннолетних"}
+        if call.data == "Alko":
+            webbrowser.open_new_tab("малаяохта.рф")
+        if call.data == "Contacts":
+            topText = "По всем интересующим вопросам Вы можете обратиться по тел. 5284663"
+            subList[1] = {"Site": "Сайт МО", "VK": "Группа VK"}
+            
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=topText, reply_markup=makeCategories(subList))
+
+
+
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-    if message.text == "Привет":
+    if message.text == "/new":
         bot.send_message(chat_id=message.from_user.id,
                      text="Выберите категорию:",
-                     reply_markup=makeCategories(),
+                     reply_markup=makeCategories(catList),
                      parse_mode='HTML')
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "/new - Новое обращение")
